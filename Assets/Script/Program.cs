e622f7a646b5a39decefe1f6debbbad21f566eb0
Лещинская Яна <147346934+Tttrrrmr@users.noreply.github.com>
2025-06-10 21:35:15 +0300

    12

--- Assets/Script/Program.cs
+++ Assets/Script/Program.cs
@@ -1,16 +1,14 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Program : MonoBehaviour
+
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public void AlgoritmGame()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
